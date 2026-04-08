d8df6461a1cff682dd5da4d56bf458581d9099cb
Rodrigo Cochran <rodrigocochran@berkeley.edu>
2026-04-08 12:41:39 -0700

    Continue on Phase 1 tests

--- tests/tests_sync.py
+++ tests/tests_sync.py
@@ -1,10 +1,25 @@
 import reqx
+from rich import print
 
 
 def test_true_200():
     client = reqx.Client()
     resp = client.get("https://httpbin.org/get")
     assert resp.status_code == 200
-    assert "Content-Type" in resp.headers
+    assert "content-type" in resp.headers
     body = resp.json()
     assert body["url"] == "https://httpbin.org/get"
+    print(body)
+
+
+def test_400():
+    client = reqx.Client()
+    resp = client.get("https://httpbin.org/status/400")
+    assert resp.status_code == 400
+    assert "content-type" in resp.headers
+    body = None
+    try:
+        body = resp.json()
+    except Exception as e:
+        assert "invalid JSON response" in str(e)
+    assert not body
