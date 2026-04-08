0c6dc1cf227e43b8777e865cef81a3c06211e128
Rodrigo Cochran <rodrigocochran@berkeley.edu>
2026-04-08 13:04:56 -0700

    Add GIL-releaese confirmation

--- tests/tests_sync.py
+++ tests/tests_sync.py
@@ -1,8 +1,11 @@
+import threading
+import time
+
 import reqx
 from rich import print
 
-HTTPBIN_HOST = "https://httpbin.org"
-# HTTPBIN_HOST = "http://localhost"
+# HTTPBIN_HOST = "https://httpbin.org"
+HTTPBIN_HOST = "http://localhost"
 HTTP_BIN_PORT = "80"
 
 
@@ -27,3 +30,34 @@ def test_400():
     except Exception as e:
         assert "invalid JSON response" in str(e)
     assert not body
+
+
+def test_gil_release():
+
+    def task(wait_time: int):
+        print(f"Starting {wait_time} second wait")
+        resp = client.get(f"{HTTPBIN_HOST}/delay/{wait_time}")
+        print(f"Finished {wait_time} second wait")
+        assert resp.status_code == 200
+        assert "content-type" in resp.headers
+
+    client = reqx.Client()
+
+    wait_time_1 = 2
+    wait_time_2 = 3
+
+    t1 = threading.Thread(target=task, args=(wait_time_1,))
+    t2 = threading.Thread(target=task, args=(wait_time_2,))
+
+    start = time.perf_counter()
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    end = time.perf_counter()
+    duration = end - start
+
+    print(f"Duration: {duration}s")
+    assert duration <= max(wait_time_1, wait_time_2) * 1.1
