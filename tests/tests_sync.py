ce25072334cecb129f4f5f016201869f5e05cc26
Rodrigo Cochran <rodrigocochran@berkeley.edu>
2026-04-08 12:45:21 -0700

    Continue on Phase 1 tests

--- tests/tests_sync.py
+++ tests/tests_sync.py
@@ -1,20 +1,24 @@
 import reqx
 from rich import print
 
+HTTPBIN_HOST = "https://httpbin.org"
+# HTTPBIN_HOST = "http://localhost"
+HTTP_BIN_PORT = "80"
+
 
 def test_true_200():
     client = reqx.Client()
-    resp = client.get("https://httpbin.org/get")
+    resp = client.get(f"{HTTPBIN_HOST}/get")
     assert resp.status_code == 200
     assert "content-type" in resp.headers
     body = resp.json()
-    assert body["url"] == "https://httpbin.org/get"
+    assert body["url"] == f"{HTTPBIN_HOST}/get"
     print(body)
 
 
 def test_400():
     client = reqx.Client()
-    resp = client.get("https://httpbin.org/status/400")
+    resp = client.get(f"{HTTPBIN_HOST}/status/400")
     assert resp.status_code == 400
     assert "content-type" in resp.headers
     body = None
